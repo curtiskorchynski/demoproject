bc763af35542a9091323fadd031fc9db755b12e8
unknown <curtis.korchynski@STATION38.datadrill.ad>
2014-06-04 10:42:50 -0600

    Second commit

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,29 @@
 var DEMO = (function(module){
 	module.DoIt = function(resultobj){
-		resultobj.prepend(Date() + '<br/>');
+		
+		
+		okay = function(){
+			// Do something
+		}
+		uploadPhoto = function(data)
+		{
+			cameraPic.src = "data:image/jpeg;base64," + data;
+			// Successful upload to the server
+			navigator.notification.alert(
+				'Your Photo has been uploaded',  // message
+				okay,                           // callback
+			    'Photo Uploaded',              // title
+			    'OK'                          // buttonName
+			);
+		
+			
+			
+			
+			
+
+		}	
+		navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+
 	}
 	
 	return module;
